95c2ba8476d33ae4d130b731ba4117e077235262
Michael Long <michaeltlong10@gmail.com>
2024-01-16 01:26:05 -0500

    make blobs circulate avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,19 @@ import avatar from "../../public/avatar.svg";
 export default function Home() {
   return (
     <Container className="min-h-screen py-32">
-      <div className="relative w-full">
-        <div className="animate-blob animation-delay-1000 absolute right-12 top-0 -z-20 h-96 w-96 rounded-full bg-sky-300 opacity-70 mix-blend-darken blur-2xl ease-linear dark:bg-sky-600 dark:opacity-30 dark:mix-blend-lighten" />
-        <div className="animate-blob absolute -right-12 top-24 -z-20 h-96 w-96 rounded-full bg-blue-300 opacity-70 mix-blend-darken blur-2xl ease-linear dark:bg-blue-600 dark:opacity-30 dark:mix-blend-lighten" />
-        <div className="animate-blob animation-delay-2000 absolute right-36 top-24 -z-20 h-96 w-96 rounded-full bg-indigo-300 opacity-70 mix-blend-darken blur-2xl ease-linear dark:bg-indigo-600 dark:opacity-30 dark:mix-blend-lighten" />
-        <div className="animate-blob animation-delay-3000 absolute right-12 top-48 -z-20 h-96 w-96 rounded-full bg-violet-300 opacity-70 mix-blend-darken blur-2xl ease-linear dark:bg-violet-600 dark:opacity-30 dark:mix-blend-lighten" />
-      </div>
       <div className="flex h-full flex-col justify-center gap-8 sm:flex-row-reverse sm:items-center md:gap-16 lg:gap-24 xl:gap-32 2xl:gap-48">
-        <Image
-          src={avatar}
-          alt="Picture of the author"
-          width={160}
-          height={160}
-          className="bg-muted border-border rounded-full border p-2"
-        />
+        <div className="relative shrink-0">
+          <Image
+            src={avatar}
+            alt="Picture of the author"
+            className="bg-muted border-border h-40 w-40 rounded-full border p-2"
+          />
+          <div className="animate-blob animation-delay-1000 absolute -left-20 bottom-0 -z-20 h-80 w-80 rounded-full bg-sky-300 opacity-70 mix-blend-darken blur-2xl ease-linear dark:bg-sky-600 dark:opacity-30 dark:mix-blend-lighten" />
+          <div className="animate-blob absolute -bottom-20 -z-20 h-80 w-80 rounded-full bg-blue-300 opacity-70 mix-blend-darken blur-2xl ease-linear dark:bg-blue-600 dark:opacity-30 dark:mix-blend-lighten" />
+          <div className="animate-blob animation-delay-2000 absolute -bottom-20 -left-40 -z-20 h-80 w-80 rounded-full bg-indigo-300 opacity-70 mix-blend-darken blur-2xl ease-linear dark:bg-indigo-600 dark:opacity-30 dark:mix-blend-lighten" />
+          <div className="animate-blob animation-delay-3000 absolute -left-20 top-0 -z-20 h-80 w-80 rounded-full bg-violet-300 opacity-70 mix-blend-darken blur-2xl ease-linear dark:bg-violet-600 dark:opacity-30 dark:mix-blend-lighten" />
+        </div>
+
         <div className="flex flex-col gap-4">
           <H1>{"hi, i'm michael."}</H1>
           <p className="text-accent-foreground">
