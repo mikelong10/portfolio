75d2b131cb0c3a1b7962bf874b603710f7ffaf3f
Michael Long <michaeltlong10@gmail.com>
2024-01-26 23:16:55 -0500

    add local resume file and download button

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,11 +1,25 @@
+import Link from "next/link";
+import { FileDown } from "lucide-react";
+
 import Container from "@components/structure/Container";
+import { Button } from "@components/ui/button";
 import Resume from "./Resume";
 
 export default function ResumePage() {
   return (
     <Container className="min-h-screen py-32">
-      <div className="flex w-full justify-center">
+      <div className="flex w-full flex-col items-center justify-center gap-8">
         <Resume />
+        <Button asChild className="flex gap-4" size={"lg"}>
+          <Link
+            href={"./Michael-Long-SWE-Resume-2024.pdf"}
+            download={"Michael-Long-Resume.pdf"}
+            target="_blank"
+          >
+            <FileDown />
+            <p className="font-extrabold">Download</p>
+          </Link>
+        </Button>
       </div>
     </Container>
   );
