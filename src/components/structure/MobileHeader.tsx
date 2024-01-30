5a3dfc0e6391d1b1abc4227456f92969f41711f6
Michael Long <michaeltlong10@gmail.com>
2024-01-29 22:53:44 -0500

    add theme toggle to mobile nav

--- src/components/structure/MobileHeader.tsx
+++ src/components/structure/MobileHeader.tsx
@@ -9,6 +9,7 @@ import H1 from "@components/typography/h1";
 import { Button } from "@components/ui/button";
 import { Separator } from "@components/ui/separator";
 import { Sheet, SheetContent, SheetTrigger } from "@components/ui/sheet";
+import ThemeToggle from "@components/utils/ThemeToggle";
 import avatar from "../../../public/avatar.svg";
 
 const mobileNavLinks = [
@@ -33,28 +34,33 @@ export default function MobileNav() {
             <Menu size={32} />
           </Button>
         </SheetTrigger>
-        <SheetContent className="flex w-full min-w-[360px] flex-col items-center gap-6 p-6">
-          <div className="flex w-full items-center justify-center gap-4 pb-4 pt-8">
-            <Link
-              href="/"
-              className="hover:animate-pulse"
-              onClick={() => setNavOpen(false)}
-            >
-              <Image
-                src={avatar}
-                alt="Picture of the author"
-                className="bg-muted border-border h-24 w-24 rounded-full border p-2"
-              />
-            </Link>
-            <div className="flex flex-col">
-              <H1 className="text-2xl sm:text-4xl lg:text-4xl">michael long</H1>
-              <p className="text-sm">think different.</p>
+        <SheetContent className="flex w-full min-w-[360px] flex-col items-center gap-12 p-6">
+          <div className="flex w-full flex-col items-center justify-center gap-2 pt-8">
+            <div className="flex items-center justify-center gap-4">
+              <Link
+                href="/"
+                className="hover:animate-pulse"
+                onClick={() => setNavOpen(false)}
+              >
+                <Image
+                  src={avatar}
+                  alt="Picture of the author"
+                  className="bg-muted border-border h-24 w-24 rounded-full border p-2"
+                />
+              </Link>
+              <div className="flex flex-col">
+                <H1 className="text-2xl sm:text-4xl lg:text-4xl">
+                  michael long
+                </H1>
+                <p className="text-sm">think different.</p>
+              </div>
             </div>
+            <ThemeToggle />
           </div>
           <Separator />
           <div className="flex h-full flex-col items-center justify-between">
             <nav className="flex flex-col items-center justify-center">
-              <ul className="flex flex-col items-center justify-between gap-8 py-8">
+              <ul className="flex flex-col items-center justify-between gap-8">
                 {mobileNavLinks.map((link) => (
                   <Button key={link.href} asChild variant={"ghost"}>
                     <Link
