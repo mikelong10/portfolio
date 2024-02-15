de195b85385e70a10c65df26d629e711b436f7d5
Michael Long <michaeltlong10@gmail.com>
2024-02-15 00:05:04 -0500

    fix 3d card for mobile

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,19 @@
+import Container from "@components/structure/Container";
+import Content from "@components/structure/Content";
+import H1 from "@components/typography/h1";
+
 export default function ProjectsPage() {
-  return <div>ProjectsPage</div>;
+  return (
+    <Container
+      className="flex min-h-screen items-center justify-center py-32"
+      noisy
+    >
+      <Content>
+        <div className="flex flex-col items-center gap-8">
+          <H1>projects</H1>
+          <p>coming soon...</p>
+        </div>
+      </Content>
+    </Container>
+  );
 }
--- src/components/3dDisplayCard.tsx
+++ src/components/3dDisplayCard.tsx
@@ -17,7 +17,7 @@ export default function ThreeDimensionalDisplayCard({
       <div className="bg-background absolute -z-20 size-full rounded-3xl" />
       <div className="bg-secondary dark:bg-secondary absolute -z-10 size-3/4 rounded-full blur-3xl dark:size-4/5" />
       <CardBody className="group/card xs:p-8 xs:rounded-2xl border-border bg-background/70 dark:bg-background/50 relative h-full w-auto rounded-xl p-6 shadow-xl sm:rounded-3xl sm:p-10 md:p-12 dark:border">
-        <CardBody className="flex h-full flex-col justify-between gap-12">
+        <CardBody className="flex h-full flex-col justify-between gap-12 pb-10">
           <div className="flex flex-col gap-4">
             <CardItem translateZ="30">
               <H3 className="text-tertiary">{title}</H3>
@@ -26,12 +26,12 @@ export default function ThreeDimensionalDisplayCard({
               <p>{description}</p>
             </CardItem>
           </div>
-          <div className="flex flex-col gap-8">
-            <CardItem translateZ="60" className="w-full">
+          <div className="flex flex-col">
+            <CardItem translateZ="60" className="mb-8 w-full">
               {image}
             </CardItem>
-            <CardItem translateZ="40" className="w-full">
-              <div className="flex items-center justify-end gap-4">
+            <CardItem translateZ="40" className="relative w-full">
+              <div className="absolute right-0 z-50 flex items-center justify-end gap-4">
                 {footerButtons}
               </div>
             </CardItem>
